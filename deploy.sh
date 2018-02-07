6ff1187756897a1141706f38b244795ae8c90fdf
Vittal Pai <vittalpai@ymail.com>
2018-02-07 14:38:19 +0530

    Add Script Skeleton

---
+++ deploy.sh
@@ -0,0 +1,64 @@
+#!/bin/bash
+#
+# Copyright 2018 IBM Corp. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the “License”);
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#  https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an “AS IS” BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# load configuration variables
+source local.env
+
+function usage() {
+  echo "Usage: $0 [--install,--uninstall,--recycle,--env]"
+}
+
+function install() {
+  echo "Installing..."
+  
+}
+
+function uninstall() {
+  echo "Uninstalling..."
+}
+
+function update() {
+  echo "Updating actions..."
+
+}
+
+function showenv() {
+  echo "PACKAGE_NAME=$PACKAGE_NAME"
+  echo "CLOUDANT_URL=$CLOUDANT_URL"
+}
+
+function recycle() {
+  uninstall
+  install
+}
+
+case "$1" in
+"--install" )
+install
+;;
+"--uninstall" )
+uninstall
+;;
+"--update" )
+update
+;;
+"--env" )
+showenv
+;;
+* )
+usage
+;;
+esac
