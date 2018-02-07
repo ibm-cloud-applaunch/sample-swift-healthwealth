366642bcd09003c0f3d3d242f55c80ea107db22b
Vittal Pai <vittalpai@ymail.com>
2018-02-07 14:26:59 +0530

    Update map location

--- HealthWealth/Views/MapViewController.swift
+++ HealthWealth/Views/MapViewController.swift
@@ -1,4 +1,4 @@
-//
+    //
 //  MapViewController.swift
 //  HealthWealth
 //
@@ -7,20 +7,32 @@
 //
 
 import UIKit
+import MapKit
 
-class MapViewController: UIViewController {
-
+class MapViewController: UIViewController, CLLocationManagerDelegate, MKMapViewDelegate  {
+    
+    @IBOutlet weak var mapView: MKMapView!
+    let homeLocation = CLLocation(latitude: 12.949343, longitude: 77.644764)
+    let regionRadius: CLLocationDistance = 200
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         self.view.backgroundColor = MenuItems.backgroundColor
-        // Do any additional setup after loading the view.
+        mapView.showsUserLocation = true
+        centerMapOnLocation(location: homeLocation)
+    }
+    
+    func centerMapOnLocation(location: CLLocation)
+    {
+        let coordinateRegion = MKCoordinateRegionMakeWithDistance(location.coordinate,
+                                                                  regionRadius * 2.0, regionRadius * 2.0)
+        mapView.setRegion(coordinateRegion, animated: true)
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
 
     @IBAction func deliverBtn(_ sender: Any) {
         let alert = UIAlertController(title: "Alert", message: "Order Successful.", preferredStyle: .alert)
